c744e91dd1dc46efedad053976847eca6289b6d8
Saro <ramitakahapana@gmail.com>
2021-01-06 15:21:46 +0700

    UPDATE controller

---
+++ app/Controllers/Http/StudentController.js
@@ -0,0 +1,55 @@
+'use strict'
+const Database = use('Database')
+const Student = use("App/Models/Student")
+
+class StudentController {
+    async index() {
+        const students = await Student.query().fetch()
+        return { status: 200, error: undefined, data: students }
+    }
+    async show() {
+        const id = params
+        const student = await Database
+            .select('*')
+            .from('students')
+            .where('student_id', id)
+            .innerJoin('universities', 'students.university_id', 'universities.university_id')
+            .first()
+        return { status: 200, error: undefined, data: student || {} }
+    }
+    async store({ request }) {
+        const data = request.body
+
+        const students = await Student.create(data)
+        return { status: 200, error: undefined, data: students }
+    }
+    async update({ request }) {
+        const { body, params } = request
+        const { id } = params
+        const { first_name, last_name, student_class, university_id } = body
+
+        const studentId = await Database
+            .table('students')
+            .where({ student_id: id })
+            .update({ first_name, last_name, student_class, university_id })
+
+        const student = await Database
+            .table('students')
+            .where({ student_id: studentId })
+            .first()
+
+        return { status: 200, error: undefined, data: student }
+    }
+    async destroy({ request }) {
+        const { id } = request.params
+
+        await Database
+            .table('students')
+            .where({ student_id: id })
+            .delete()
+
+        return { status: 200, error: undefined, data: { message: 'success' } }
+    }
+}
+
+module.exports = StudentController
---
+++ app/Controllers/Http/UniversityController.js
@@ -0,0 +1,56 @@
+'use strict'
+const Database = use('Database')
+const University = use("App/Models/University")
+
+class UniversityController {
+    async index() {
+        const universities = await University.query().fetch()
+        return { status: 200, error: undefined, data: universities }
+    }
+    async show({ request }) {
+        const { id } = request.params
+        const university = await Database
+            .select('*')
+            .from('universities')
+            .where("university_id", id)
+            .first()
+
+        return { status: 200, error: undefined, data: university || {} }
+    }
+    async store({ request }) {
+        const data = request.body
+
+        const universities = await University.create(data)
+        return { status: 200, error: undefined, data: universities }
+    }
+
+    async update({ request }) {
+        const { body, params } = request
+        const { id } = params
+        const { university_name } = body
+
+        const universityId = await Database
+            .table('universities')
+            .where({ university_id: id })
+            .update({ university_name })
+
+        const universitys = await Database
+            .table('universities')
+            .where({ university_id: universityId })
+            .first()
+
+        return { status: 200, error: undefined, data: universitys }
+    }
+    async destroy({ request }) {
+        const { id } = request.params
+
+        await Database
+            .table('universities')
+            .where({ university_id: id })
+            .delete()
+
+        return { status: 200, error: undefined, data: { message: 'success' } }
+    }
+}
+
+module.exports = UniversityController
