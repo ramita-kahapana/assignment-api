4afe348e0fe7c8ab7554bbcf898be0204f581a30
Saro <ramitakahapana@gmail.com>
2021-01-07 15:34:53 +0700

    UPDATE student controller

--- app/Controllers/Http/StudentController.js
+++ app/Controllers/Http/StudentController.js
@@ -13,7 +13,6 @@ class StudentController {
     async show({ request }) {
         const { id } = request.params
         const data = await Student.query().with('university').where({ student_id: id }).fetch()
-
         return { status: 200, error: undefined, data: data }
     }
     async store({ request }) {
@@ -21,7 +20,7 @@ class StudentController {
         const { first_name, last_name, student_class, university_name } = body
         const student = await Student.create({ first_name, last_name, student_class })
         const university = await University.query().where({ university_name }).fetch().then(response => JSON.parse(JSON.stringify(response)))
-        const testData = university.map(item => item.id)
+        const testData = university.map(item => item.university_id)
         let testId = await Student.query().count('student_id as id').then(response => JSON.parse(JSON.stringify(response[0])))
         const universitystudent = await UniversityStudent.create({ student_id: testId.id, university_id: testData[0] })
 
@@ -30,12 +29,12 @@ class StudentController {
     async update({ request }) {
         const { body, params } = request
         const { id } = params
-        const { first_name, last_name, student_class, university_id } = body
+        const { first_name, last_name, student_class } = body
 
         const studentId = await Database
             .table('students')
             .where({ student_id: id })
-            .update({ first_name, last_name, student_class, university_id })
+            .update({ first_name, last_name, student_class })
 
         const student = await Database
             .table('students')
