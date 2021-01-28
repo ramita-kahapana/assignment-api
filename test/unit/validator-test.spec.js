e512df6b1d53ff5428be6aba75bccacf0bb018b5
ramita <ramitakahapana@gmail.com>
2021-01-28 23:56:45 +0700

    EDIT validator test

--- test/unit/validator-test.spec.js
+++ test/unit/validator-test.spec.js
@@ -5,7 +5,7 @@ const UniversityValidator = require('../../service/UniversityValidator')
 const StudentValidator = require('../../service/StudentValidator')
 
 //University
-test('should return error unidefined when data is not entered from University validator', async ({ assert }) => {
+test('should return error unidefined when data is not entered from university validator', async ({ assert }) => {
   const universityData = await UniversityValidator({
     university_name: '',
   })
@@ -20,4 +20,13 @@ test('should return error unidefined when data is not entered from student valid
     student_class: 'sss',
   })
   assert.isArray(studentData.error, undefined)
+})
+
+test('should return only  one error if  two incorrect data is passed from student validator', async({ assert }) => {
+  const studentData = await StudentValidator({
+    first_name: '',
+    last_name: '',
+    student_class: 'sss',
+  })
+  assert.equal(studentData.error.length, 2)
 })
